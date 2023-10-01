d397d7b9f687c87af4205aa0646c669de67322f7
Pilar Muñoz <86972128+pibmunoz@users.noreply.github.com>
2023-09-30 21:15:38 -0300

    feat: add mongodb connection fix: export default filterData function

--- filterData.mjs
+++ filterData.mjs
@@ -3,7 +3,7 @@
  * @param {Array} data
  * @returns {Array} filteredUsers
  */
-export function filterData(data) {
+export default function filterData(data) {
     const usernameRegex = /([aeiou])/g;
     const filteredUsers = data.filter((user) => user.username.match(usernameRegex).length >= 3);
     return filteredUsers;
