219d15a24ec3c439d38d9325c6e20b2b9b6fa992
Pilar Muñoz <86972128+pibmunoz@users.noreply.github.com>
2023-10-02 09:54:12 -0300

    docs: add comments explaining purpose of variables and runDatabase function

--- database.mjs
+++ database.mjs
@@ -1,14 +1,18 @@
 import { MongoClient } from "mongodb";
 import 'dotenv/config'
 
+// Connection details for the MongoDB database
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
-
 const dbName = process.env.MONGODB_DB_NAME;
 
+// Collection names
 const collectionName_1 = process.env.MONGODB_COLLECTION_NAME_1;
 const collectionName_2 = process.env.MONGODB_COLLECTION_NAME_2;
 
+/**
+ * Connects to the MongoDB database and returns the client and collections.
+ */
 async function runDatabase() {
     await client.connect();
     console.log("Connected successfully to server :D!");
