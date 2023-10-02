49aaed85c3a247f7381c9f11b2e2924af729ebaa
Pilar Muñoz <86972128+pibmunoz@users.noreply.github.com>
2023-10-02 09:57:00 -0300

    docs: add comments to explain filterUsers function fix: export default to maintain cohesive structure

--- filterUsers.mjs
+++ filterUsers.mjs
@@ -1,10 +1,12 @@
 /**
- * 
+ * Filters users by username to only include users with at least 3 vowels
  * @param {Array} data
  * @returns {Array} filteredUsers
  */
-export default function filterUsers(data) {
+function filterUsers(data) {
     const usernameRegex = /([aeiou])/g;
     const filteredUsers = data.filter((user) => user.username.match(usernameRegex).length >= 3);
     return filteredUsers;
 }
+
+export default filterUsers;
